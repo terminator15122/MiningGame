5c8d8d640d1648db5ba9d94ca3a042b9243cac7d
Medved21 <54591928+Medved21@users.noreply.github.com>
2019-08-28 00:35:50 +0700

    12

---
+++ MiningGameUnity/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int MineConst;
+    static private float MinerStorage;
+    public int Miners;
+    public int MinerMax;
+    public float MinerUpgrade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MineConst = 1;
+        MinerUpgrade = 1;
+        MinerStorage = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MinerStorage <= MinerMax)
+        { 
+        MinerStorage =( MinerStorage + (MineConst * Time.deltaTime * Miners * MinerUpgrade));
+        }
+
+        
+
+        
+
+
+    }
+    void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 200, 50), "Кол-во куды в шахте: " + Mathf.Round(MinerStorage));
+        GUI.Box(new Rect(0, 50, 200, 100), "Кол-во апгрейды: " + MinerUpgrade);
+        GUI.Box(new Rect(0, 100, 200, 150), "Кол-во майнеров: " + Miners);
+    }
+
+    public void BuyUpgrade()
+    {
+        if (MinerUpgrade < 30 && MinerStorage > 5)
+        {
+            MinerUpgrade++;
+            MinerStorage -= 5;
+        }
+
+    }
+    public void BuyMiner()
+    {
+        if (Miners < 30 && MinerStorage > 5)
+        {
+            Miners++;
+            MinerStorage -= 5;
+        }
+
+    }
+
+
+
+}
