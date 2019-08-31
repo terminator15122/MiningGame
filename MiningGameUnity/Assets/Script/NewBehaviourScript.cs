6bfd91cd4bb9cec236d849fb721b3b122d0269c3
Medved21 <54591928+Medved21@users.noreply.github.com>
2019-08-31 20:50:48 +0700

    123

--- MiningGameUnity/Assets/Script/NewBehaviourScript.cs
+++ MiningGameUnity/Assets/Script/NewBehaviourScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -9,13 +10,36 @@ public class NewBehaviourScript : MonoBehaviour
     public int Miners;
     public int MinerMax;
     public float MinerUpgrade;
+    public float UpgradeCostRab;
+    public float UpgradeCostUp;
+    public float UpgradeCostStor;
+    private float n;
+    private float k;
+    private float y;
+    public float constan;
+    public Text upgradeText1;
+    public Text upgradeText2;
+    public Text upgradeText3;
+    string UpgradeCostUpstr;
+    string UpgradeCostRabstr;
+    string UpgradeCostStorstr;
     // Start is called before the first frame update
     void Start()
     {
         MineConst = 1;
         MinerUpgrade = 1;
         MinerStorage = 0;
-    }
+        UpgradeCostRab = 5;
+        UpgradeCostStor = 50;
+        UpgradeCostUp = 5;
+        n = 1;
+        k = 1;
+        y = 1;
+        constan = 2;
+        MinerMax = 200;
+        ;
+        
+}
 
     // Update is called once per frame
     void Update()
@@ -24,6 +48,10 @@ public class NewBehaviourScript : MonoBehaviour
         { 
         MinerStorage =( MinerStorage + (MineConst * Time.deltaTime * Miners * MinerUpgrade));
         }
+        upgradeText1.text = UpgradeCostRabstr;
+        upgradeText2.text = UpgradeCostStorstr;
+        upgradeText3.text = UpgradeCostUpstr;
+
     }
     void OnGUI()
     {
@@ -34,30 +62,41 @@ public class NewBehaviourScript : MonoBehaviour
 
     public void BuyUpgrade()
     {
-        if (MinerUpgrade < 30 && MinerStorage > 5)
+        if (MinerUpgrade < 30 && MinerStorage > UpgradeCostUp)
         {
             MinerUpgrade++;
-            MinerStorage -= 5;
+            MinerStorage -= UpgradeCostUp;
+            n++;
+            UpgradeCostUp *=Mathf.Pow(constan,n);
+            UpgradeCostUpstr = UpgradeCostUp.ToString(); 
         }
-
+        
     }
     public void BuyStorage()
     {
-        if (MinerMax < 1500 && MinerStorage >50)
+        if (MinerMax < 1500 && MinerStorage >UpgradeCostStor)
         {
             MinerMax = MinerMax + 50 ;
-            MinerStorage -= 50;
+            MinerStorage -= UpgradeCostStor;
+            y++;
+            UpgradeCostStor *= Mathf.Pow(constan, y);
+            UpgradeCostStorstr = UpgradeCostStor.ToString();
         }
+
+
+
 
     }
     public void BuyMiner()
     {
-        if (Miners < 30 && MinerStorage > 5)
+        if (Miners < 30 && MinerStorage > UpgradeCostRab)
         {
             Miners++;
-            MinerStorage -= 5;
+            MinerStorage -= UpgradeCostRab;
+            UpgradeCostRab*= Mathf.Pow(constan, k);
+            UpgradeCostRabstr = UpgradeCostRab.ToString();
         }
 
 
     }
-    }
+}
