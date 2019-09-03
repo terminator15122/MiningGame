9cb89000dc860385ab718e84f4d0650fe6867f94
terminator15122 <terminator15122@gmail.com>
2019-09-03 18:36:20 +0700

    Created Game Manager

---
+++ MiningGameUnity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static public int minerCount;
+    static public float minerStorage;
+    static public float minerStorageMax;
+    static public float upgradeCostMiner;
+    static public float upgradeCost;
+    static public float upgradeCostStorage;
+    static public int upgradeCount;
+    static public int storageUpgradeCount;
+    static public float constant;
+
+
+    void Start()
+    {
+        minerCount = 1;
+        minerStorage = 0f;
+        minerStorageMax = 100f;
+        upgradeCostMiner = 10f;
+        upgradeCost = 10f;
+        upgradeCostStorage = 50f;
+        upgradeCount = 1;
+        storageUpgradeCount = 1;
+        constant = 1.15f;
+    }
+
+
+    void Update()
+    {
+        if (minerStorage <= minerStorageMax)
+        {
+            minerStorage += (Time.deltaTime * minerCount * upgradeCount);
+        }
+        else
+        {
+            minerStorage = minerStorageMax;
+        }
+    }
+}
---
+++ MiningGameUnity/Assets/Scripts/Main.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Main : MonoBehaviour
+{
+    public Text upgradeTextUpgrade;
+    public Text upgradeTextMiner;
+    public Text upgradeTextStorage;
+         
+
+    void Update()
+    {
+        upgradeTextUpgrade.text = GameManager.upgradeCost.ToString();
+        upgradeTextMiner.text = GameManager.upgradeCostMiner.ToString();
+        upgradeTextStorage.text = GameManager.upgradeCostStorage.ToString();
+    }
+
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 200, 50), "Кол-во руды в шахте: " + Mathf.Round(GameManager.minerStorage));
+        GUI.Box(new Rect(0, 50, 200, 50), "Кол-во апгрейды: " + GameManager.upgradeCount);
+        GUI.Box(new Rect(0, 100, 200, 50), "Кол-во майнеров: " + GameManager.minerCount);
+    }
+
+
+    public void BuyUpgrade()
+    {
+        if (GameManager.upgradeCount < 30 && GameManager.minerStorage > GameManager.upgradeCost)
+        {
+            GameManager.upgradeCount++;
+            GameManager.minerStorage -= GameManager.upgradeCost;
+            GameManager.upgradeCost *= Mathf.Pow(GameManager.constant, GameManager.upgradeCount);
+        }
+    }
+
+
+    public void BuyStorage()
+    {
+        if (GameManager.minerStorageMax < 1500 && GameManager.minerStorage > GameManager.upgradeCostStorage)
+        {
+            GameManager.minerStorageMax = GameManager.minerStorageMax + 50;
+            GameManager.minerStorage -= GameManager.upgradeCostStorage;
+            GameManager.upgradeCostStorage *= Mathf.Pow(GameManager.constant, GameManager.storageUpgradeCount);
+            GameManager.storageUpgradeCount++;
+        }
+    }
+
+
+    public void BuyMiner()
+    {
+        if (GameManager.minerCount < 30 && GameManager.minerStorage > GameManager.upgradeCostMiner)
+        {
+            GameManager.minerCount++;
+            GameManager.minerStorage -= GameManager.upgradeCostMiner;
+            GameManager.upgradeCostMiner *= Mathf.Pow(GameManager.constant, GameManager.minerCount);
+        }
+    }
+}
